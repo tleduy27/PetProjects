bc13a164017dad6ff5d96db7ec4a11c15f6aa8b3
tleduy27 <tleduy27@gmail.com>
2026-03-04 09:44:05 +0700

    feat: add create room API

---
+++ PetProject2026/PetProject2026/Controllers/RoomsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetProject2026.Context;
+using PetProject2026.DTOs;
+using PetProject2026.Models;
+
+namespace PetProject2026.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly BookingContext _bookingContext;
+        public RoomsController(BookingContext bookingContext)
+        {
+            _bookingContext = bookingContext;
+        }
+
+        [HttpPost("create-room")]
+        public async Task<ActionResult<Room>> CreateRoom(CreateRoomDto request)
+        {
+            if (string.IsNullOrEmpty(request.rName))
+                return BadRequest("Name is required");
+            if (request.rPrice <= 0) return BadRequest("Price must be greater than 0");
+            var room = new Room
+            {
+                roomName = request.rName,
+                roomPrice = request.rPrice
+            };
+            _bookingContext.rooms.Add(room);
+            await _bookingContext.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(GetRoomById),
+                new { id = room.roomId },
+                room
+                );
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Room>> GetRoomById(int id)
+        {
+            var room = await _bookingContext.rooms.FindAsync(id);
+
+            if (room == null)
+                return NotFound();
+
+            return Ok(room);
+        }
+    }
+}
--- PetProject2026/PetProject2026/Program.cs
+++ PetProject2026/PetProject2026/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PetProject2026.Context;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<BookingContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MyConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
